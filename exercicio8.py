3f5c2827382e4673c461250cc6326ae7cae0a6a2
arthurvillaca2020 <arthur.villaca.jf@hotmail.com>
2024-04-03 19:21:34 -0300

    gf

---
+++ exercicio8.py
@@ -0,0 +1,5 @@
+quantidadedekmrodado = int(input("Digite a quantidade de KM rodados: "))
+quantidadedecombustivelconsumido = int(input("Digite a quantidade de combustivel consumido em litros: "))
+mediadekmporlitro = quantidadedekmrodado / quantidadedecombustivelconsumido
+print("Media de consumo (KM/L)")
+print(mediadekmporlitro)
